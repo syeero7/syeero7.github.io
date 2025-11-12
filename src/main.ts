05ba2d0e6e3713f1f6ec0f11bd1d6203b0f0614e
emal <emalhasitha@proton.me>
2025-11-13 05:16:14 +0530

    chore: remove old files

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { initializeAnimation } from "./lib/particle-animation";
-import { initializeNavbarHandler } from "./lib/nav-menu";
-import "./style.css";
-
-window.addEventListener("DOMContentLoaded", () => {
-  initializeAnimation();
-  initializeNavbarHandler();
-});
