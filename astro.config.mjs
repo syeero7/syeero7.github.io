534390bde9d224b9303e5ceed31ff1e083299975
emal <emalhasitha@proton.me>
2025-11-19 04:43:56 +0530

    Revert "chore: configure CD"
    This reverts commit c475d509e7e0a7a4abce34ace3ef9f858cf9ef92.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://syeero7.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
