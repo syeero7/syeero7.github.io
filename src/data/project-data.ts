869c2cdddd4048ae4026d72a3228f9f7e8c407e5
emal <emalhasitha@proton.me>
2025-11-19 14:00:14 +0530

    chore: update project information

--- src/data/project-data.ts
+++ src/data/project-data.ts
@@ -11,18 +11,17 @@ export const projects: Project[] = [];
 
 projects.push({
   title: "File Crow",
-  source: "https://github.com",
-  image: "https://placehold.co/768x432.png",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim",
+  source: "https://github.com/syeero7/file-crow",
+  image: "/project-file-crow.webp",
+  description: "A file server for sharing files over a local network via HTTP.",
   languages: ["Go"],
 });
 projects.push({
-  title: "Lorem Ipsum",
-  source: "https://github.com",
-  image: "https://placehold.co/768x432.png",
-  view: "https.example.com",
+  title: "Chess",
+  source: "https://github.com/syeero7/chess-game",
+  image: "/project-chess.webp",
+  view: "https://xchess.netlify.app",
   description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim",
+    "A client-side chess game featuring offline modes for player vs player and player vs AI gameplay.",
   languages: ["TypeScript", "HTML", "CSS"],
 });
